fc8da84cafab9e830bf272cf37b43a300c6b39f5
amirmardnai <amirmardnai@users.noreply.github.com>
2026-08-16 12:21:26 +0330

    Import BorderStroke in ScannerScreen

--- app/src/main/java/com/app/bound/screens/ScannerScreen.kt
+++ app/src/main/java/com/app/bound/screens/ScannerScreen.kt
@@ -1,6 +1,7 @@
 package com.app.bound.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
